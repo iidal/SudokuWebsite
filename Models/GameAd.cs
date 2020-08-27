e68ecef0f954721e580be85617f9345389123381
Iida <iida.lintuaho@gmail.com>
2020-08-27 19:48:18 +0300

    fixed git ignore oops

---
+++ Models/GameAd.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace SudokuWebsite.Models
+{
+    public class GameAd
+    {
+
+        public string Id  { get; set; }
+
+        [JsonPropertyName("img")]
+        public string Image  { get; set; }
+        public string Url  { get; set; }
+        public string Title  { get; set; }
+        public string Description  { get; set; }
+
+
+
+        public override string ToString() => JsonSerializer.Serialize<GameAd>(this);
+        
+    }
+}
